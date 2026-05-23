c2e450ae1735b1a47035f583aee959487395e717
Lucas Oliveira <dsojoaolucas@gmail.com>
2026-05-23 00:23:34 -0300

    updates 0.0.0.5

--- capivarinhas.py
+++ capivarinhas.py
@@ -3,8 +3,11 @@ import pygame
 import os
 import questionary
 
-
 pygame.init()
+
+def musica(musica):
+    pygame.mixer.music.load(f"{musica}.mp3")
+    pygame.mixer.music.play()
 def iniciar():
     print("A Venum's Game...")
     sleep(2)
@@ -39,15 +42,43 @@ def menu():
     elif opcao == "Sair do jogo":
         return 3
 def jogo():
+    barra = ''
+    for c in range(101):
+        print(f"\rCarregando {c}%: |{barra}|", end='')
+        if c%2 == 0:
+            barra+="■"
+        sleep(0.05)
+    pygame.mixer.music.stop()
     os.system("cls")
-    print("Carregando")
-    for c in range(100):
-        sleep(1)
-        print(c)
+    sleep(1)
+    nome = str(input("Qual o seu nome, capivara?\n")).strip()
+    inicio = f"bem, {nome}, essa jornada será mais que lendária..."
+    anima1 = ""
+    pygame.mixer.music.load("teclado.mp3")
+    pygame.mixer.music.play()
+    sleep(0.8)
+    for letra in inicio:
+        anima1 += letra
+        print(f"\r{anima1}",end='')
+        sleep(0.08)
+        if len(anima1) == 5 + len(nome):
+            pygame.mixer.music.stop()
+            sleep(1.2)
+            pygame.mixer.music.load("teclado.mp3")
+            pygame.mixer.music.play()
+            sleep(0.8)
+    pygame.mixer.music.stop()
+    sleep(2)
+    os.system("cls")
+    
+    input()
+
+
+
+while True:
+    iniciar()
+    if menu() == 1:
+        jogo()
+    elif menu() == 2:
+        pygame.mixer.music.stop()
         os.system("cls")
-
-
-iniciar()
-menu()
-if menu == 1:
-    jogo()
